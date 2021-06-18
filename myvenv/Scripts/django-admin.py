67b1f169f5a5e98afeb5b69be89693be4e8c5888
Cici Woolf <60899330+ciciwoolf@users.noreply.github.com>
2021-06-18 17:55:23 -0500

    Added updates

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\chris\google drive\development projects\djangogirls\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
